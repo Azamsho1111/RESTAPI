cb79a982e57ad21586f09240b48b9693b5786631
Azamsho1111 <dmrhelp.1@yandex.com>
2025-06-02 05:29:07 +0500

    Update API routes with all filter endpoints

--- routes/api.php
+++ routes/api.php
@@ -4,105 +4,114 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DashboardController;
-use App\Http\Controllers\Api\ModelsController;
 use App\Http\Controllers\Api\UsersController;
 use App\Http\Controllers\Api\CategoriesController;
+use App\Http\Controllers\Api\ModelsController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Публичные маршруты
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-// Auth routes (public)
-Route::prefix('auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::get('/user', [AuthController::class, 'user']);
-    });
-});
-
-// Protected routes
+// Защищенные маршруты
 Route::middleware('auth:sanctum')->group(function () {
-    
-    // Dashboard routes
+    // Авторизация
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::get('/auth/user', [AuthController::class, 'user']);
+
+    // Дашборд
     Route::get('/dashboard/stats', [DashboardController::class, 'getStats']);
-    
-    // Models routes
-    Route::apiResource('models', ModelsController::class);
-    Route::patch('/models/{id}/status', [ModelsController::class, 'updateStatus']);
-    Route::post('/models/{id}/moderate', [ModelsController::class, 'moderate']);
-    
-    // Users routes  
-    Route::apiResource('users', UsersController::class);
-    Route::patch('/users/{id}/block', [UsersController::class, 'block']);
-    Route::patch('/users/{id}/unblock', [UsersController::class, 'unblock']);
-    
-    // Categories and filters routes
-    Route::get('/categories', [CategoriesController::class, 'getCategories']);
-    Route::post('/categories', [CategoriesController::class, 'createCategory']);
-    Route::put('/categories/{id}', [CategoriesController::class, 'updateCategory']);
-    Route::delete('/categories/{id}', [CategoriesController::class, 'deleteCategory']);
-    
-    Route::get('/sections', [CategoriesController::class, 'getSections']);
-    Route::post('/sections', [CategoriesController::class, 'createSection']);
-    Route::put('/sections/{id}', [CategoriesController::class, 'updateSection']);
-    Route::delete('/sections/{id}', [CategoriesController::class, 'deleteSection']);
-    
-    Route::get('/materials', [CategoriesController::class, 'getMaterials']);
-    Route::post('/materials', [CategoriesController::class, 'createMaterial']);
-    Route::put('/materials/{id}', [CategoriesController::class, 'updateMaterial']);
-    Route::delete('/materials/{id}', [CategoriesController::class, 'deleteMaterial']);
-    
-    Route::get('/renders', [CategoriesController::class, 'getRenders']);
-    Route::post('/renders', [CategoriesController::class, 'createRender']);
-    Route::put('/renders/{id}', [CategoriesController::class, 'updateRender']);
-    Route::delete('/renders/{id}', [CategoriesController::class, 'deleteRender']);
-    
-    Route::get('/colors', [CategoriesController::class, 'getColors']);
-    Route::post('/colors', [CategoriesController::class, 'createColor']);
-    Route::put('/colors/{id}', [CategoriesController::class, 'updateColor']);
-    Route::delete('/colors/{id}', [CategoriesController::class, 'deleteColor']);
-    
-    Route::get('/softs', [CategoriesController::class, 'getSofts']);
-    Route::post('/softs', [CategoriesController::class, 'createSoft']);
-    Route::put('/softs/{id}', [CategoriesController::class, 'updateSoft']);
-    Route::delete('/softs/{id}', [CategoriesController::class, 'deleteSoft']);
-    
-    Route::get('/formats', [CategoriesController::class, 'getFormats']);
-    Route::post('/formats', [CategoriesController::class, 'createFormat']);
-    Route::put('/formats/{id}', [CategoriesController::class, 'updateFormat']);
-    Route::delete('/formats/{id}', [CategoriesController::class, 'deleteFormat']);
-    
-    Route::get('/polygons', [CategoriesController::class, 'getPolygons']);
-    Route::post('/polygons', [CategoriesController::class, 'createPolygon']);
-    Route::put('/polygons/{id}', [CategoriesController::class, 'updatePolygon']);
-    Route::delete('/polygons/{id}', [CategoriesController::class, 'deletePolygon']);
-    
-    Route::get('/styles', [CategoriesController::class, 'getStyles']);
-    Route::post('/styles', [CategoriesController::class, 'createStyle']);
-    Route::put('/styles/{id}', [CategoriesController::class, 'updateStyle']);
-    Route::delete('/styles/{id}', [CategoriesController::class, 'deleteStyle']);
-    
-    Route::get('/animation', [CategoriesController::class, 'getAnimation']);
-    Route::post('/animation', [CategoriesController::class, 'createAnimation']);
-    Route::put('/animation/{id}', [CategoriesController::class, 'updateAnimation']);
-    Route::delete('/animation/{id}', [CategoriesController::class, 'deleteAnimation']);
-    
-    Route::get('/others', [CategoriesController::class, 'getOthers']);
-    Route::post('/others', [CategoriesController::class, 'createOther']);
-    Route::put('/others/{id}', [CategoriesController::class, 'updateOther']);
-    Route::delete('/others/{id}', [CategoriesController::class, 'deleteOther']);
+    Route::get('/dashboard/recent-models', [DashboardController::class, 'getRecentModels']);
+    Route::get('/dashboard/recent-users', [DashboardController::class, 'getRecentUsers']);
+
+    // Пользователи
+    Route::get('/users', [UsersController::class, 'index']);
+    Route::post('/users', [UsersController::class, 'store']);
+    Route::get('/users/{id}', [UsersController::class, 'show']);
+    Route::put('/users/{id}', [UsersController::class, 'update']);
+    Route::delete('/users/{id}', [UsersController::class, 'destroy']);
+
+    // Модели
+    Route::get('/models', [ModelsController::class, 'index']);
+    Route::post('/models', [ModelsController::class, 'store']);
+    Route::get('/models/{id}', [ModelsController::class, 'show']);
+    Route::put('/models/{id}', [ModelsController::class, 'update']);
+    Route::delete('/models/{id}', [ModelsController::class, 'destroy']);
+
+    // Категории и фильтры
+    Route::prefix('categories')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getCategories']);
+        Route::post('/', [CategoriesController::class, 'createCategory']);
+        Route::put('/{id}', [CategoriesController::class, 'updateCategory']);
+        Route::delete('/{id}', [CategoriesController::class, 'deleteCategory']);
+    });
+
+    Route::prefix('sections')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getSections']);
+        Route::post('/', [CategoriesController::class, 'createSection']);
+        Route::put('/{id}', [CategoriesController::class, 'updateSection']);
+        Route::delete('/{id}', [CategoriesController::class, 'deleteSection']);
+    });
+
+    Route::prefix('materials')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getMaterials']);
+        Route::post('/', [CategoriesController::class, 'createMaterial']);
+        Route::put('/{id}', [CategoriesController::class, 'updateMaterial']);
+        Route::delete('/{id}', [CategoriesController::class, 'deleteMaterial']);
+    });
+
+    Route::prefix('renders')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getRenders']);
+        Route::post('/', [CategoriesController::class, 'createRender']);
+        Route::put('/{id}', [CategoriesController::class, 'updateRender']);
+        Route::delete('/{id}', [CategoriesController::class, 'deleteRender']);
+    });
+
+    Route::prefix('colors')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getColors']);
+        Route::post('/', [CategoriesController::class, 'createColor']);
+        Route::put('/{id}', [CategoriesController::class, 'updateColor']);
+        Route::delete('/{id}', [CategoriesController::class, 'deleteColor']);
+    });
+
+    Route::prefix('softs')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getSofts']);
+        Route::post('/', [CategoriesController::class, 'createSoft']);
+        Route::put('/{id}', [CategoriesController::class, 'updateSoft']);
+        Route::delete('/{id}', [CategoriesController::class, 'deleteSoft']);
+    });
+
+    Route::prefix('formats')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getFormats']);
+        Route::post('/', [CategoriesController::class, 'createFormat']);
+        Route::put('/{id}', [CategoriesController::class, 'updateFormat']);
+        Route::delete('/{id}', [CategoriesController::class, 'deleteFormat']);
+    });
+
+    Route::prefix('polygons')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getPolygons']);
+        Route::post('/', [CategoriesController::class, 'createPolygon']);
+        Route::put('/{id}', [CategoriesController::class, 'updatePolygon']);
+        Route::delete('/{id}', [CategoriesController::class, 'deletePolygon']);
+    });
+
+    Route::prefix('styles')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getStyles']);
+        Route::post('/', [CategoriesController::class, 'createStyle']);
+        Route::put('/{id}', [CategoriesController::class, 'updateStyle']);
+        Route::delete('/{id}', [CategoriesController::class, 'deleteStyle']);
+    });
+
+    Route::prefix('animation')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getAnimation']);
+        Route::post('/', [CategoriesController::class, 'createAnimation']);
+        Route::put('/{id}', [CategoriesController::class, 'updateAnimation']);
+        Route::delete('/{id}', [CategoriesController::class, 'deleteAnimation']);
+    });
+
+    Route::prefix('others')->group(function () {
+        Route::get('/', [CategoriesController::class, 'getOthers']);
+        Route::post('/', [CategoriesController::class, 'createOther']);
+        Route::put('/{id}', [CategoriesController::class, 'updateOther']);
+        Route::delete('/{id}', [CategoriesController::class, 'deleteOther']);
+    });
 });
